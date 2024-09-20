b2abbeb39107b47e4a73244f69b52e5e3c9c630c
Vlad <petrenkov785@gmail.com>
2024-09-20 18:05:55 +0200

    [1]

---
+++ exam/films/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    director = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.DurationField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name='movies')
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+    poster = models.ImageField(upload_to='posters/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
